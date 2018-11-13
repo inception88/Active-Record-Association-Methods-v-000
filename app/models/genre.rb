fdbaff048588843209a7c169ca0b3327bf4203df
Benjamin Jones <bljones36@gmail.com>
2018-11-13 02:09:55 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,7 +3,7 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    self.sons.size
   end
 
   def artist_count
